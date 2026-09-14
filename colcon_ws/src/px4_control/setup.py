9032ad5d73f528eafd12b5f6dc31ed16d2b9eed2
Saif Khan <110802306+K-saif@users.noreply.github.com>
2026-09-14 21:59:10 +0530

    Update setup.py

--- colcon_ws/src/px4_control/setup.py
+++ colcon_ws/src/px4_control/setup.py
@@ -25,8 +25,10 @@ setup(
     entry_points={
         'console_scripts': [
 		'square_mission = px4_control.square_mission:main',
+        'square_mission_sr = px4_control.square_mission_sr:main',
         'gesture_camera = px4_control.gesture_camera:main',
-        'drone_camera_viewer = px4_control.drone_camera_viewer:main'
+        'drone_camera_viewer = px4_control.drone_camera_viewer:main',
+        'ai_target_follower = px4_control.ai_target_follower:main'
         ],
     },
 )
